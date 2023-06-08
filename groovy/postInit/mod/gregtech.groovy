d97850a23e89155acb79c94954a5ee5329e2655e
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-06-08 18:21:48 +0400

    Changed earlygame progression
    - Added Tinkered Hegemony
    - Added Gravestone mod
    - Changed recipe for crafting station and workbench

--- groovy/postInit/mod/gregtech.groovy
+++ groovy/postInit/mod/gregtech.groovy
@@ -10,4 +10,18 @@ crafting.shapedBuilder()
         .replace()
         .register()
 
+crafting.shapedBuilder()
+        .name('workbench')
+        .output(metaitem('workbench'))
+        .matrix('CsC',
+                'PcP',
+                'PsP')
+        .key('C', ore('chestWood'))
+        .key('S', ore('toolSaw'))
+        .key('s', ore('slabWood'))
+        .key('P', ore('plankWood'))
+        .key('c', item('tconstruct:tooltables'))
+        .replace()
+        .register()
+
 crafting.replaceShapeless('compressed_clay', metaitem('compressed.clay'), [metaitem('wooden_form.brick'), item('minecraft:clay')])
