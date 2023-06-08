ccf0bbb2085714f152a7ab31e5626a00a57343ea
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-06-08 15:56:12 +0400

    AE2 changes
    - Hidden AE2 facades from JEI (temporary)
    - Removed quartz grindstone and its recipes

--- groovy/prePostInit/removal.groovy
+++ groovy/prePostInit/removal.groovy
@@ -4,9 +4,11 @@ mods.jei.yeet(item('libvulpes:productgear'))
 mods.jei.yeet(item('libvulpes:productrod'))
 mods.jei.yeet(item('libvulpes:productnugget'))
 mods.jei.yeet(item('advancedrocketry:platepress'))
+mods.jei.yeet(item('appliedenergistics2:grindstone')) //Add recipes for nether quartz dust and flour using either mortar or macerator. Probably macerator
 
 //Removes categories from JEI
 mods.jei.hideCategory('zmaster587.AR.platePresser')
+mods.jei.hideCategory('appliedenergistics2.grinder')
 
 //Remove recipes
 def recipesToRemove = [
