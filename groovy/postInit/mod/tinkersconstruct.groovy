40af7d0b2f946c949f73366ea40dd9aab0f345c8
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-06-09 18:08:14 +0400

    Rainbow sap and TCo pattern recipes changed

--- groovy/postInit/mod/tinkersconstruct.groovy
+++ groovy/postInit/mod/tinkersconstruct.groovy
@@ -1,4 +1,5 @@
 //crafting.replaceShapeless('groutbricks', item('engreggant:grout_brick'), [metaitem('wooden_form.brick'), item('tconstruct:soil:0')])
 crafting.replaceShapeless('crafting_station', item('tconstruct:tooltables'), [ore('toolSaw'), ore('craftingTableWood')])
+crafting.replaceShapeless('pattern', item('tconstruct:pattern:0'), [item('minecraft:paper'), item('minecraft:paper'), ore('stickWood'), ore('stickWood')])
 
 //furnace.add(item('engreggant:grout_brick'), item('tconstruct:materials:0'), 0.5f)
