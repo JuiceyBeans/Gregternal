090cdbe27540f34513dbc60ed870544eda001853
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-06-21 18:51:15 +0400

    Snad recipe

---
+++ groovy/postInit/mod/snad.groovy
@@ -0,0 +1,2 @@
+crafting.replaceShapeless('snad', item('snad:snad:0'), [metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustClay'), item('minecraft:dye:15'), item('minecraft:dye:15'), item('minecraft:dye:15'), item('minecraft:dye:15')])
+crafting.replaceShapeless('red_snad', item('snad:snad:1'), [item('snad:snad:0'), item('minecraft:sand:1')])
