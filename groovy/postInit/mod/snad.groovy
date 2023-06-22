0bc7c0ec887b681ce2bd55e7e5e5d661df514807
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-06-22 11:36:44 +0400

    Fixed bone meal recipe for Snad

--- groovy/postInit/mod/snad.groovy
+++ groovy/postInit/mod/snad.groovy
@@ -1,4 +1,4 @@
 crafting.replaceShapeless('snad', item('snad:snad:0'), [metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustClay'), item('minecraft:dye:15'), item('minecraft:dye:15'), item('minecraft:dye:15'), item('minecraft:dye:15')])
-crafting.replaceShapeless('apatite_snad', item('snad:snad:0'), [metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustClay'), metaitem('dustApatite'), metaitem('dustApatite')])
+crafting.addShapeless('apatite_snad', item('snad:snad:0'), [metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustQuartzSand'), metaitem('dustClay'), metaitem('dustApatite'), metaitem('dustApatite')])
 
 crafting.replaceShapeless('red_snad', item('snad:snad:1'), [item('snad:snad:0'), item('minecraft:sand:1')])
